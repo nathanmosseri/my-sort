bd931fcf0167c037bc4c1de51437d823f6aed3ec
Nathan <namo678@me.com>
2022-08-22 12:03:52 -0400

    added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import React, { useState } from "react";
+import Login from "./Login";
+import MainPage from './MainPage'
 
 function App() {
+
+  const [loginSuccess, setLoginSuccess] = useState(true)
+
+
   return (
     <div className="App">
+      {loginSuccess ? <MainPage /> : <Login/>}
+
       
     </div>
   );
